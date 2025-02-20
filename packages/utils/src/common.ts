d1590a4faa68ba1e965ad0be501ddec1a0f23448
Vanisper <273266469@qq.com>
2025-02-20 08:12:11 +0800

    feat(utils): 🚀 新增 `camelCase` 函数，实现串烧命名转小驼峰

--- packages/utils/src/common.ts
+++ packages/utils/src/common.ts
@@ -2,3 +2,8 @@ import process from 'node:process'
 
 export const isWindows
   = typeof process !== 'undefined' && process.platform === 'win32'
+
+// 串烧命名转小驼峰
+export function camelCase(name: string) {
+  return name.replace(/-(\w)/g, (_, c) => c.toUpperCase())
+}
