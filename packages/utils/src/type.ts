bd25d2e06c1b54f0efa193b7ef25c6247594f92f
Vanisper <273266469@qq.com>
2025-02-22 01:35:44 +0800

    feat(utils): 🚀 新增 `strEnum` 和 `numEnum` 函数，增强枚举类型处理功能

--- packages/utils/src/type.ts
+++ packages/utils/src/type.ts
@@ -1,3 +1,5 @@
+import type { MapArray, MapTuple } from '@https-enable/types'
+
 export interface IsStreamOptions {
   /**
    * When this option is `true`, the method returns `false` if the stream has already been closed.
@@ -11,4 +13,20 @@ export function isStream(stream: any, { checkOpen = true }: IsStreamOptions = {}
     && typeof stream === 'object'
     && !!(stream.writable || stream.readable || !checkOpen || (stream.writable === undefined && stream.readable === undefined))
     && typeof stream.pipe === 'function'
+}
+
+export function strEnum<T extends string>(o: Array<T>): { [K in T]: K } {
+  return o.reduce((res, key) => {
+    res[key] = key
+    return res
+  }, Object.create(null))
+}
+
+export function numEnum<T extends string>(arr: Array<T>): MapArray<T>
+export function numEnum<const T extends readonly string[]>(arr: T): MapTuple<T>
+export function numEnum(arr: any[]): any {
+  return arr.reduce((acc, key, index) => {
+    acc[key] = index
+    return acc
+  }, {})
 }
